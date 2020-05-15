81a2487243be90da3af001e3a5642f86ae291581
Anmaree <anmaree.osmond@gmail.com>
2020-05-14 23:26:28 -0600

    added notes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /**
  * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
- * TODO: Complete searchCity() to get user input and get data using getWeatherData()
- * TODO: Complete showWeatherData() to set the data in the the html file from response
+ * Completed getWeatherData() to return json response Promise
+ * Completed searchCity() to get user input and get data using getWeatherData()
+ * Completed showWeatherData() to set the data in the the html file from response
  */
 
 // API_KEY for maps api
@@ -10,21 +10,12 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 /**
  * Retrieve weather data from openweathermap
- * HINT: Use fetch()
- * HINT: URL should look like this: 
+ * Use fetch()
+ * URL should look like this: 
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
-/*
-getWeatherData = (city) => {
-  const URL = "https://api.openweathermap.org/data/2.5/weather";
-  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
-  const weatherPromise  = fetch(FULL_URL);
-  return weatherPromise.then((response) => {
-    return response.json();
-  })
-}
-*/
- //===============================================
+
+
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //Use template literals to create a url with input and an API key
@@ -38,8 +29,8 @@ getWeatherData = (city) => {
 
 
 /**
- * Retrieve city input and get the weather data
- * HINT: Use the promise returned from getWeatherData()
+ Retrieve city input--the id is what is in html and then look for the data in the browser > inspect > console
+ Use the promise returned from getWeatherData()
  */
 searchCity = () => {
   const city = document.getElementById('city-input').value;
@@ -55,8 +46,7 @@ searchCity = () => {
 }
 
 /**
- * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
+the data below is found in the browser under inspect
  */
 showWeatherData = (weatherData) => {
  document.getElementById("city-name").innerText = weatherData.name
