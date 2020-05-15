cbbf0e096b26935233a15ef4bf981389810c3f44
Anmaree <anmaree.osmond@gmail.com>
2020-05-14 23:14:44 -0600

    completed

--- script.js
+++ script.js
@@ -14,22 +14,29 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
  * HINT: URL should look like this: 
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
+/*
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
-  //HINT: Use template literals to create a url with input and an API key
-
-  const FULL_URL = `${URL}?q${city}&appid=${API_KEY}&units=imperial`
-  console.log(FULL_URL)
-
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise  = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
+}
+*/
+ //===============================================
+getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  //Use template literals to create a url with input and an API key
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`
   const weatherPromise = fetch(FULL_URL);
-  weatherPromise.then((response)=>{
-  
-  console.log(response)
+  return weatherPromise.then((response)=>{
+    return response.json();
   })
 
 }
 
-getWeatherData("detroit")
+
 /**
  * Retrieve city input and get the weather data
  * HINT: Use the promise returned from getWeatherData()
@@ -37,6 +44,13 @@ getWeatherData("detroit")
 searchCity = () => {
   const city = document.getElementById('city-input').value;
   // CODE GOES HERE
+  getWeatherData(city)
+  .then((response)=>{
+    console.log(response)
+    showWeatherData(response)
+  }).catch((error)=>{
+    console.log(error)
+  })
 
 }
 
@@ -45,7 +59,10 @@ searchCity = () => {
  * HINT: make sure to console log the weatherData to see how the data looks like
  */
 showWeatherData = (weatherData) => {
-  //CODE GOES HERE
-  
+ document.getElementById("city-name").innerText = weatherData.name
+ document.getElementById("weather-type").innerText = weatherData.weather[0].main
+ document.getElementById("temp").innerText = weatherData.main.temp
+ document.getElementById("min-temp").innerText = weatherData.main.temp_min
+ document.getElementById("max-temp").innerText = weatherData.main.temp_max
 }
 
